5d6b64206f68869f1d669d15e9d8faee18a62b60
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-03-13 13:05:38 +0100

    backend code commit by ENGR. IPAYE

---
+++ springboot-backend/src/main/java/dev/engripaye/springbootbackend/entity/User.java
@@ -0,0 +1,27 @@
+package dev.engripaye.springbootbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role;
+
+}
