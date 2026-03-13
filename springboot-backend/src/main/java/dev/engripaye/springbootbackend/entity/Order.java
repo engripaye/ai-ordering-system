fb430481e1491dcf1f4374f67060b6344f3668b4
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-03-13 13:08:24 +0100

    backend code commit by ENGR. IPAYE

---
+++ springboot-backend/src/main/java/dev/engripaye/springbootbackend/entity/Order.java
@@ -0,0 +1,30 @@
+package dev.engripaye.springbootbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String item;
+
+    private int quantity;
+
+    private String status;
+
+    private Long userId;
+}
